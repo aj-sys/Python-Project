96081d24cee960165ce08f0c34d4340a8a3a1d30
aj811 <ajara811@aol.com>
2020-11-25 21:22:34 +0000

    My first task in python

---
+++ Modules.py
@@ -0,0 +1,13 @@
+#Modules.py
+import function
+function.showdata("Ajara is my name","I am learning python")
+
+function.greetings('Mariam')
+import function as fn
+fn.showdata('A.J', 'Anita is in the car')
+
+import function
+print(dir(function))
+
+import math
+print(dir(math.cos(90))
---
+++ task1.py
@@ -0,0 +1,44 @@
+#task1.py
+#elif statements 
+
+
+days = []
+day = input('Please enter the day you were born: ')
+gender = input('Are you  a male or female? ')
+
+
+if day == 'Monday':
+    if gender == 'male':
+        print('Your day born name is Kwadwo.')
+    else:
+        print('Your day born name is Adwoa.')
+if day == 'Tuesday':
+    if gender == 'male':
+        print('Your day born name is Kwabena')
+    else:
+        print('Your day born name is Abena')
+if day == 'Wednesday':
+    if gender == 'male':
+        print('Your day born name is Kwaku')
+    else:
+        print('Your day born name is Akua')
+if day == 'Thursday':
+    if gender == 'male':
+        print('Your day born name is Yaw')
+    else:
+        print('Your day born name is Yaa')
+if day == 'Friday':
+    if gender == 'male':
+        print('Your day born name is Kofi')
+    else:
+        print('Your day born name is Afia')
+if day == 'Saturday':
+    if gender == 'male':
+        print('Your day born name is Kwame')
+    else:
+        print('Your day born name is Ama')
+if day == 'Sunday':
+    if gender == 'male':
+        print('Your day born name is Kwesi')
+    else:
+        print('Your day born name is Akosua')
