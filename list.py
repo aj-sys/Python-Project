ed00e37a943dc9419b31cf1684e04bf860099692
aj811 <ajara811@aol.com>
2020-11-25 20:43:01 +0000

    My second project

---
+++ list.py
@@ -0,0 +1,66 @@
+name= "America"
+print(name)
+favFoods= ["banku","konkonte","waakye","beans","tuozaafi"]
+print(favFoods)
+
+print(favFoods[-1])
+print(favFoods[0:4])
+print("My favourite foods are "+ favFoods[0] + " "  + favFoods[1] + " ")
+string1=     "We are"
+string2= " here to study"
+string3=" cloud computing"
+print(string1 + string2 + string3)
+print(favFoods[0].title())
+
+ages=[1,2,3,4, "abi"]
+print(ages[0])
+print(str(ages[0])+str(ages[1]))
+print(ages[0],ages[4])
+
+favFoods[2]="amala"
+print(favFoods)
+ages[-1]=5
+print(ages)
+favFoods.append("eba")
+favFoods.append("pepper soup")
+print(favFoods)
+favFoods.insert(1,"rice")
+print(favFoods)
+print([1])
+# list
+attendeeList=["Anita","Linda","Akua","Rita"]
+print(attendeeList)
+#To use delete, pop and remove to take an item out from the list
+attendeeList.remove("Linda")
+print(attendeeList)
+# delete method
+attendeeList=["Anita","Linda","Akua","Rita"]
+del attendeeList[0]
+print(attendeeList)
+# pop method. Takes out the last item in the list
+attendeeList=["Anita","Linda","Akua","Rita"]
+attendeeList.pop()
+print(attendeeList)
+# If an item is popped out, it can be assigned a variable and print it out later
+attendeeList=["Anita","Linda","Akua","Rita"]
+notattendeeList=attendeeList.pop()
+print(notattendeeList)
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
