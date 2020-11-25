e2d00faa598f08d2423cc6cce9ca451ed336988c
aj811 <ajara811@aol.com>
2020-11-25 21:04:32 +0000

    my conditionals code

---
+++ conditionals.py
@@ -0,0 +1,25 @@
+#conditionals.py
+#using input statement
+
+myinput=input("enter a string")
+print("you entered entered" + myinput)
+
+#if else condition
+age = input("Enter the age")
+age = int(age)
+if age > 18:
+    print("you are legal")
+elif age == 18:
+    
+    print("you are recently legal")   
+else:
+    print("you are illegal")
+
+
+
+
+
+
+
+ 
+
